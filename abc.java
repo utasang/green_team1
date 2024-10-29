6bef96bd7bd092f28e2c1c2f84efdd62ecbe037b
UTA_VIRUS <green_sy@outlook.com>
2024-10-30 02:16:23 +0900

    Update abc.java

--- abc.java
+++ abc.java
@@ -1,6 +1,12 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+
 then, can i use this hub to workspace? test
 sysout wow legendary,
 
 1. how about lib and jdk
 
 2. and conn to sql
+
+maybe it wokrs lib, but 
+
+https://github.com/openjdk/jdk
